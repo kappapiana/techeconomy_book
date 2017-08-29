4fe2185b7a3b33e5bfe908d39c82db6e376afc26
kappapiana <carlo@piana.eu>
2017-08-29 14:22:59 +0200

    Rifatto commit, era un link simbolico, no buono

--- make/makebook.sh
+++ make/makebook.sh
@@ -1,1 +1,12 @@
-../../../../utility/makebook.sh
+#!/bin/bash
+
+cat ../*.md >  $1.md
+
+
+pandoc -N --toc -Ss -c ../css/pandoc.css -o $1.pdf ../0*.md ../1*.md ../metadata.yaml --latex-engine=xelatex
+
+pandoc -N --toc -Ss -c ../css/pandoc.css -o $1.html ../0*.md ../1*.md  ../metadata.yaml
+
+pandoc -N --toc -Ss -c ../css/pandoc.css -o $1_self-contained.html ../0*.md ../1*.md ../metadata.yaml  --self-contained
+
+pandoc -N --toc -Ss -c ../css/pandoc.css -o $1.epub ../0*.md ../1*.md ../metadata.yaml
